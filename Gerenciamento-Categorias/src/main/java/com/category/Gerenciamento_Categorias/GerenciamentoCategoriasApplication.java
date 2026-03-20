fabb1f7ac1d18867863b1dcc0b6b0ff5bdbfc266
Prevs-997 <mthsnetwork@gmail.com>
2026-03-20 16:39:18 -0300

    Models Criados

---
+++ Gerenciamento-Categorias/src/main/java/com/category/Gerenciamento_Categorias/GerenciamentoCategoriasApplication.java
@@ -0,0 +1,13 @@
+package com.category.Gerenciamento_Categorias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoCategoriasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoCategoriasApplication.class, args);
+	}
+
+}
---
+++ Gerenciamento-Categorias/src/main/java/com/category/Gerenciamento_Categorias/Models/CategoriaModel.java
@@ -0,0 +1,35 @@
+package com.category.Gerenciamento_Categorias.Models; // Ajusta o pacote base consoante o que geraste
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria")
+public class CategoriaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(name = "nome_da_categoria")
+    private String nome;
+
+
+    @Column(name = "descricao_detalhada")
+    private String descricao;
+
+    public CategoriaModel() {
+    }
+
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getDescricao() { return descricao; }
+
+    public void setDescricao(String descricao) { this.descricao = descricao; }
+}
